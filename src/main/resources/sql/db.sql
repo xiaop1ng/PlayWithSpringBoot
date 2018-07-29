1060a1fdd8a6569a735daa2c6818a487f3709da5
xiaop1ng <784516419@qq.com>
2018-07-29 22:49:05 +0800

    fix jdk 9 JAXB API bug

--- src/main/resources/sql/db.sql
+++ src/main/resources/sql/db.sql
@@ -9,7 +9,7 @@ create table `contract_template` (
 	version varchar(128), -- 模板版本号
 	template_type varchar(128), -- 模板类型
 	create_time datetime not null default now(), -- 创建时间
-	state tinyint(4) not null default 1, -- 状态 1. 禁用 2. 启用
+	enable boolean not null default false, -- 状态 f. 禁用 t. 启用
 	doc_src varchar(512), -- word 文档
 	pdf_src	varchar(512) -- pdf 文档
 );
