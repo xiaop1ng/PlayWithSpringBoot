13328c3fcd427811d2e109c0b317bc237014d876
xiaop1ng <784516419@qq.com>
2018-07-31 18:18:01 +0800

    '文档操作'

---
+++ src/main/java/com/xiaoping/utils/DocxUtils.java
@@ -0,0 +1,74 @@
+package com.xiaoping.utils;
+
+import org.docx4j.openpackaging.exceptions.Docx4JException;
+import org.docx4j.openpackaging.packages.WordprocessingMLPackage;
+import org.docx4j.wml.ContentAccessor;
+import org.docx4j.wml.Text;
+
+import javax.xml.bind.JAXBElement;
+import java.io.File;
+import java.io.IOException;
+import java.util.ArrayList;
+import java.util.List;
+
+public class DocxUtils {
+
+    private static WordprocessingMLPackage tmp;
+    private static List<Object> texts = null;
+    private final static int MODE_REPLACE_ALL = 0;
+    private final static int MODE_REPLACE_ONCE = 1;
+
+    private static WordprocessingMLPackage load(String path) throws Docx4JException {
+        WordprocessingMLPackage template = WordprocessingMLPackage.load(new File(path));
+        tmp = template;
+        return template;
+    }
+
+
+    private static List<Object> getElements(Object obj, Class<?> toSearch) {
+        List<Object> result = new ArrayList<>();
+        if (obj instanceof JAXBElement) {
+            obj = ((JAXBElement<?>) obj).getValue();
+        }
+        if (obj.getClass().equals(toSearch)) {
+            result.add(obj);
+        } else if (obj instanceof ContentAccessor) {
+            List<?> children = ((ContentAccessor) obj).getContent();
+            children.stream()
+                    .forEach(child -> {
+                        result.addAll(getElements(child, toSearch));
+                    });
+        }
+        return result;
+    }
+
+    private static void attr(String placeholder, String value){
+        attr(placeholder, value, MODE_REPLACE_ONCE);
+    }
+
+    private static void attr(String placeholder, String value, int mode) {
+        if(texts == null) {
+            texts = getElements(tmp.getMainDocumentPart(), Text.class);// holder
+        }
+        for (Object text : texts) {
+            Text textElement = (Text) text;
+            if (textElement.getValue().contains(placeholder)) {
+                textElement.setValue(textElement.getValue().replace(placeholder, value));
+                if(mode == MODE_REPLACE_ONCE) break;
+            }
+        }
+    }
+
+    private static void save(String target) throws Docx4JException {
+        File f = new File(target);
+        tmp.save(f);
+    }
+
+    public static void main(String[] args) throws IOException, Docx4JException {
+        WordprocessingMLPackage wordTemplate = load("src/main/resources/template.docx");
+        attr("{{Line1}}", "Hello");
+        attr("{{Line2}}", "World!");
+        attr("{{Name}}", "Xiaop1ng", MODE_REPLACE_ALL);
+        save("src/main/resources/tempFile.docx");
+    }
+}
