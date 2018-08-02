f2a1b984fc12b5f885016e34de2d3b1dec4a405c
xiaop1ng <784516419@qq.com>
2018-08-02 14:53:48 +0800

    JPA的使用

--- src/main/java/com/xiaoping/controller/api/ContractTemplateController.java
+++ src/main/java/com/xiaoping/controller/api/ContractTemplateController.java
@@ -4,8 +4,7 @@ import com.xiaoping.dao.ContractTemplateDao;
 import com.xiaoping.entity.ContractTemplate;
 import com.xiaoping.pojo.Rs;
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
 
 import java.util.Date;
 
@@ -17,13 +16,40 @@ public class ContractTemplateController {
     private ContractTemplateDao contractTemplateDao;
 
 
-    @RequestMapping("/save")
-    public Rs saveContracTemplate(){
+    @PostMapping("/save")
+    public Rs saveContracTemplate() {
         ContractTemplate template = new ContractTemplate();
         template.setTemplateName("劳务合同");
         template.setVersion("V1.0.1");
         template.setEnable(true);
         template.setCreateTime(new Date());
+        return Rs.ok(contractTemplateDao.save(template));
+    }
+
+    @GetMapping("/types")
+    public Rs types() {
+        return Rs.ok(contractTemplateDao.types());
+    }
+
+    @GetMapping("/list")
+    public Rs list() {
+        return Rs.ok(contractTemplateDao.findAll());
+    }
+
+    @PostMapping("/del")
+    public Rs delete(@RequestParam long id){
+        contractTemplateDao.deleteById(id);
+        return Rs.ok();
+    }
+
+    @PostMapping("/upsert")
+    public Rs upsert(@RequestParam(required = false) Long id,
+                     @RequestParam(required = true) String templateName) {
+        ContractTemplate template = new ContractTemplate();
+        template.setTemplateName(templateName);
+        if(id != null){
+            template.setId(id);
+        }
         return Rs.ok(contractTemplateDao.save(template));
     }
 
--- src/main/java/com/xiaoping/dao/ContractTemplateDao.java
+++ src/main/java/com/xiaoping/dao/ContractTemplateDao.java
@@ -1,14 +1,18 @@
 package com.xiaoping.dao;
 
 import com.xiaoping.entity.ContractTemplate;
+import org.springframework.data.jpa.repository.Query;
 import org.springframework.data.repository.CrudRepository;
 import org.springframework.stereotype.Repository;
 
 import javax.persistence.Table;
+import java.util.List;
 
 
 @Repository
 @Table(name = "contract_template")
 public interface ContractTemplateDao extends CrudRepository<ContractTemplate, Long> {
 
+    @Query(value = "SELECT DISTINCT(template_type) from contract_template", nativeQuery = true)
+    public List<String> types();
 }
