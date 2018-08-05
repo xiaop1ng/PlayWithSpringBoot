98b391fe5828d2af7cc2090915d204e671602ed6
xiaop1ng <784516419@qq.com>
2018-08-05 15:56:15 +0800

    log4j

--- src/main/java/com/xiaoping/controller/api/HomeController.java
+++ src/main/java/com/xiaoping/controller/api/HomeController.java
@@ -1,14 +1,20 @@
 package com.xiaoping.controller.api;
 
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
 public class HomeController {
+
 	
 	@RequestMapping("/")
 	String home() {
 		return "Hello, SpringBoot!";
 	}
+
+//	@GetMapping("/starter")
+//	String starter(){return helloService.sayHello();}
 
 }
