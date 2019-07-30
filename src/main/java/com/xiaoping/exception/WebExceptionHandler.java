4ed5873354380c3e5fd4d8c8ba75a8a18df528c0
xiaop1ng <784516419@qq.com>
2019-07-30 19:32:28 +0800

    auth

--- src/main/java/com/xiaoping/exception/WebExceptionHandler.java
+++ src/main/java/com/xiaoping/exception/WebExceptionHandler.java
@@ -1,6 +1,8 @@
 package com.xiaoping.exception;
 
 import com.auth0.jwt.exceptions.JWTVerificationException;
+import com.auth0.jwt.exceptions.TokenExpiredException;
+import com.xiaoping.constant.Constans;
 import com.xiaoping.pojo.Rs;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
@@ -28,6 +30,9 @@ public class WebExceptionHandler {
 			return Rs.err(ex.getErr(), ex.getMessage());
 		} else if (e instanceof JWTVerificationException) {
 			return Rs.err(Rs.ERROR_CODE_UNAUTHORIZED, "验证授权出错！");
+		} else if (e instanceof TokenExpiredException) {
+			res.setHeader(Constans.HEADER_AUTHORIZATION, null);
+			throw new InvokeException(Rs.ERROR_CODE_AUTHORIZED_TIMEOUT, "会话已过期，请重新登录！");
 		}
 		return Rs.errMsg("系统错误");
 	}
